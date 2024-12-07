1e9c582058739db810ba2afe6b427d7bb5c0842a
mabdullahk3 <muh.abdullahk12@gmail.com>
2024-12-07 20:12:20 +0500

    Minor changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-// Database Connection
+
 connectDB();
 
 // Routes
@@ -39,5 +39,5 @@ app.use('/api/topics', topicRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/admin', adminRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
