01c091f07e512db67c942a0e6c1fc30a9ed6b555
mabdullahk3 <muh.abdullahk12@gmail.com>
2024-12-08 12:12:12 +0500

    old commit

--- app.js
+++ app.js
@@ -39,5 +39,5 @@ app.use('/api/topics', topicRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/admin', adminRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
